40d903d2337d3157a912e005ac0933f4a3ccc9dd
shunyavkash-deep-bhimani <deep.bhimani@shunyavkash.com>
2023-12-11 16:14:26 +0530

    created authentication pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ErrorBoundry from "./component/ErrorBoundry";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CustomSnack from "./component/ui/CustomSnack";
 import Home from "./page/Home";
 import Manager from "./page/Manager";
 import Clients from "./page/Clients";
 import Projects from "./page/Projects";
 import Invoices from "./page/Invoices";
 import Profile from "./page/Profile";
-import CustomSnack from "./component/ui/CustomSnack";
+import SignUp from "./page/SignUp";
+import SignIn from "./page/SignIn";
+import ForgotPassword from "./page/ForgotPassword";
+import ConfirmPassword from "./page/ConfirmPassword";
 
 const routes = createBrowserRouter([
   { path: "/", Component: Home },
@@ -16,6 +20,10 @@ const routes = createBrowserRouter([
   { path: "/projects", Component: Projects },
   { path: "/invoices", Component: Invoices },
   { path: "/profile", Component: Profile },
+  { path: "/signup", Component: SignUp },
+  { path: "/signin", Component: SignIn },
+  { path: "/forgotpassword", Component: ForgotPassword },
+  { path: "/confirmpassword", Component: ConfirmPassword },
 ]);
 export default function Routes() {
   return (
