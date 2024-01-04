cee70f231ec8bb78633685f74029d727f3130a5f
Sujit Hirapara <sujit.hirapara@shunyavkash.com>
2024-01-04 18:45:59 +0530

    Add account page wip

--- src/page/AccountAdd.jsx
+++ src/page/AccountAdd.jsx
@@ -1,43 +1,28 @@
 import React, { useState } from "react";
 import SideBar from "../component/SideBar";
-import useApi from "../hooks/useApi";
-import { useSnack } from "../hooks/store/useSnack";
 import { useAuth } from "../hooks/store/useAuth";
 import Header from "../component/Header";
 import {
   Box,
   Button,
+  Card,
+  Form,
   FormControl,
   FormControlLabel,
   Grid,
   Link,
-  Paper,
   Radio,
   RadioGroup,
   Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   TextField,
-  Tooltip,
   Typography,
 } from "@mui/material";
-import ModalComponent from "../component/ModalComponent";
 import PlusIcon from "@mui/icons-material/Close";
 
 function AccountAdd() {
   let [sideBarWidth, setSidebarWidth] = useState("240px");
   const [showSidebar, setShowSidebar] = useState(false);
-  const [dashboardData, setDashboardData] = useState(false);
-  const { apiCall, isLoading } = useApi();
-  const { setSnack } = useSnack();
   const { accessToken, user } = useAuth();
-
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
 
   return (
     <>
@@ -87,64 +72,67 @@ function AccountAdd() {
               </Typography>
             </Box>
           </Box>
-          <Grid container rowSpacing={2.5} columnSpacing={2.5} mt={0}>
-            <Grid item xs={6} md={3} lg={3}>
-              <Box p={3} sx={{ backgroundColor: "white", borderRadius: 3 }}>
-                <Typography
-                  sx={{ color: "#2a4062", fontWeight: 500, opacity: 0.5 }}
+          <Card
+            sx={{
+              p: 3,
+              mt: 3,
+              border: 0,
+              borderRadius: 3,
+              boxShadow: "none",
+            }}
+          >
+            <Box component="form">
+              <Grid container rowSpacing={2.5} columnSpacing={2.5}>
+                <Grid
+                  item
+                  xs={12}
+                  md={12}
+                  lg={12}
+                  sx={{ "> .MuiFormControl-root": { margin: 0 } }}
                 >
-                  Total Sales
-                </Typography>
-                <Typography
-                  sx={{ fontSize: 22, color: "black", fontWeight: 600, mt: 2 }}
+                  <FormControl
+                    sx={{ "& > .MuiFormControl-root": { width: "100%" } }}
+                  >
+                    <RadioGroup
+                      aria-labelledby="demo-radio-buttons-group-label"
+                      // defaultValue="Credit"
+                      name="radio-buttons-group"
+                      sx={{
+                        width: "100%",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <FormControlLabel
+                        value="credit"
+                        control={<Radio />}
+                        label="Credit"
+                      />
+                      <FormControlLabel
+                        value="debit"
+                        control={<Radio />}
+                        label="Debit"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  md={6}
+                  lg={6}
+                  sx={{ "> .MuiFormControl-root": { margin: 0 } }}
                 >
-                  ₹1,50,000
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={6} md={3} lg={3}>
-              <Box p={3} sx={{ backgroundColor: "white", borderRadius: 3 }}>
-                <Typography
-                  sx={{ color: "#2a4062", fontWeight: 500, opacity: 0.5 }}
-                >
-                  Total Expance
-                </Typography>
-                <Typography
-                  sx={{ fontSize: 22, color: "black", fontWeight: 600, mt: 2 }}
-                >
-                  ₹80,000
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={6} md={3} lg={3}>
-              <Box p={3} sx={{ backgroundColor: "white", borderRadius: 3 }}>
-                <Typography
-                  sx={{ color: "#2a4062", fontWeight: 500, opacity: 0.5 }}
-                >
-                  Total Balance
-                </Typography>
-                <Typography
-                  sx={{ fontSize: 22, color: "black", fontWeight: 600, mt: 2 }}
-                >
-                  ₹50,000
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={6} md={3} lg={3}>
-              <Box p={3} sx={{ backgroundColor: "white", borderRadius: 3 }}>
-                <Typography
-                  sx={{ color: "#2a4062", fontWeight: 500, opacity: 0.5 }}
-                >
-                  Total Income
-                </Typography>
-                <Typography
-                  sx={{ fontSize: 22, color: "black", fontWeight: 600, mt: 2 }}
-                >
-                  ₹565000
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Required"
+                    defaultValue="Hello World"
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+          </Card>
         </Box>
       </Box>
     </>
